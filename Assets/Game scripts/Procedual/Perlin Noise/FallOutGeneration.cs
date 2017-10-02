49cd0b50163ef680ebccc307938e28ded72c0f6a
Frederik98347 <fredemandrup@hotmail.com>
2017-10-02 13:59:56 +0200

    Fallout generation ( island with water gen)
    Starting on gen values

---
+++ Assets/Game scripts/Procedual/Perlin Noise/FallOutGeneration.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FallOutGeneration {
+
+	float [,] map = new float[,] [size, size];
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			float x = i / (float)size * 2 - 1;
+			float y = j / (float)size * 2 - 1;
+		}
+	}
+}
