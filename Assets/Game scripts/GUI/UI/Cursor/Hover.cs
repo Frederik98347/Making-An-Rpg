177de992f5c0b2d4e0f7cdcb4f14d080cbf11d0a
Frederik-Pc\Frederik <Fredemandrup@hotmail.com>
2018-02-28 06:59:41 +0100

    Art
    Art updates & UI

---
+++ Assets/Game scripts/GUI/UI/Cursor/Hover.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    public Texture2D defaultTexture;
+    public Texture2D LootTexture;
+    public Texture2D BattleTexture;
+    public Texture2D VendorTexture;
+
+    public CursorMode curMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.SetCursor(defaultTexture, hotSpot, curMode);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnMouseEnter()
+    {
+        if (gameObject.tag == "Enemy")
+        {
+            Cursor.SetCursor(BattleTexture, hotSpot, curMode);
+        }
+
+        Cursor.SetCursor(defaultTexture, hotSpot, curMode);
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(defaultTexture, hotSpot, curMode);
+    }
+}
