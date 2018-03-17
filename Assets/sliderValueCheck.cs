b83a3310b85827c3e74e7a1de7e3444a7265714e
Frederik-Pc\Frederik <Fredemandrup@hotmail.com>
2018-03-17 20:48:45 +0100

    Updating to unity 2018.b
    Updating ui with a char creation screen

---
+++ Assets/sliderValueCheck.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class sliderValueCheck : MonoBehaviour {
+
+    [Header("Slider")]
+    [SerializeField] Slider Slider;
+    [SerializeField] GameObject Loader;
+    [SerializeField] string loadingObject = "Loading";
+
+    [Header("Buttons")]
+    public GameObject NextButton;
+    public GameObject BackButton;
+
+	// Use this for initialization
+	void Start () {
+        if (Loader == null)
+        {
+            Loader = GameObject.Find(loadingObject);
+        }
+        if (Slider == null)
+        {
+            Slider = Loader.GetComponentInChildren<Slider>();
+        }
+
+        NextButton.gameObject.SetActive(false);
+        BackButton.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        SetButtonsActive();
+	}
+
+    void SetButtonsActive ()
+    {
+        if (Slider.value >= 1)
+        {
+            NextButton.gameObject.SetActive(true);
+            BackButton.gameObject.SetActive(true);
+        }
+    }
+}
